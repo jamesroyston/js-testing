1c42b7faea8a0194ef9c92ad6fc6019222f878cb
jroyston <jroyston@cars.com>
2019-06-02 00:50:08 -0500

    covered sinon

--- Mocha/controllers/auth.controller.js
+++ Mocha/controllers/auth.controller.js
@@ -1,11 +1,18 @@
 function AuthController(){
 
     var roles;
+    var user;
     function setRoles(role){
         roles = role;
+        user.roles.role;
+    }
+    function setUser(inUser){
+        user = inUser;
     }
     function isAuthorized(neededRole){
-        return roles.indexOf(neededRole) >= 0; 
+        if(user) {
+            return user.isAuthorized(neededRole);
+        }
     }
 
     function isAuthorizedAsync(neededRole, cb){
@@ -16,9 +23,18 @@ function AuthController(){
         return new Promise(function(resolve){
             setTimeout(function(){resolve(roles.indexOf(neededRole) >= 0)}, 0); 
         });
-         
     }
-    return {isAuthorized, isAuthorizedAsync, setRoles, isAuthorizedPromise};
+    function getIndex(req, res){
+        if(req.user.isAuthorized('admin')){
+            return res.render('index');
+        }
+        res.render('error');
+    }
+    // function unTestedFn() {
+    //     console.log('untested fn ran');
+    // }
+    console.log('testing a statement outside of a fn');
+    return {isAuthorized, isAuthorizedAsync, setRoles, isAuthorizedPromise, getIndex, setUser};
 }
 
 module.exports = AuthController();
--- Mocha/test/controllers/auth.controller.spec.js
+++ Mocha/test/controllers/auth.controller.spec.js
@@ -3,17 +3,31 @@ var authController = require('../../controllers/auth.controller');
 var expect = require('chai').expect;
 var chai = require('chai')
 var chaiAsPromised = require('chai-as-promised')
+var should = require('chai').should();
+var sinon = require('sinon')
 chai.use(chaiAsPromised)
+chai.should()
 
 
 describe('~~~authController', function() {
-    beforeEach(function beforeEachFn() {
-                authController.setRoles(['user'])
-            })
+
     describe('isAuthorized', function() {
+        var user = {}
+        beforeEach(function() {
+            user = {
+                roles: ['user'],
+                isAuthorized: function(neededRole) {
+                    return this.roles.indexOf(neededRole) >= 0;
+                }
+            }
+            sinon.spy(user, 'isAuthorized');
+            authController.setUser(user);
+        })
         it('should return false if not auth', function() {
-            assert.equal(false, authController.isAuthorized('admin'))
+            // assert.equal(false, authController.isAuthorized('admin'))
             var isAuth = authController.isAuthorized('admin');
+            console.log(user.isAuthorized)
+            user.isAuthorized.calledOnce.should.be.true
             expect(isAuth).to.be.false;
             console.log('is admin authorized? ', authController.isAuthorized('admin'));
         })
@@ -23,7 +37,6 @@ describe('~~~authController', function() {
             console.log('is user authorized? ', authController.isAuthorized('user'));
         })
 
-        it('should not make gets when not authorized');
         
     })
 
@@ -48,4 +61,55 @@ describe('~~~authController', function() {
         })
     })
 
+    describe.only('getIndex', function() {
+        var user = {}
+        beforeEach(function() {
+            user = {
+                roles: ['user'],
+                isAuthorized: function(neededRole) {
+                    return this.roles.indexOf(neededRole) >= 0;
+                }
+            }
+        })
+
+
+            // beforeEach(function() {
+            //     sandbox = sinon.sandbox.create();
+            // })
+            // afterEach(function() {
+            //     sandbox.restore()
+            // })
+
+            // we call authcontroller.getindex
+            // but we need to pass things into getIndex....
+            // ie req and res
+            it('should render index if authorized', function() {
+                var isAuth = sinon.stub(user, 'isAuthorized').returns(true)
+                var req = { user: user}
+                var res = {
+                    render: sinon.spy()
+                }
+                var spy = res.render;
+                // NOTE: in v1, console log would show the function call data
+                authController.getIndex(req, res);
+                // console.log(spy)
+                // this returns [function] instead of the functions values
+                console.log('spy arguments', spy.args)
+                isAuth.calledOnce.should.be.true
+                spy.calledOnce.should.be.true
+                spy.firstCall.args[0].should.equal('index')
+            })
+        })
 });
+
+/*
+    Notes Section
+    we can use 
+        var func = sinon.spy() to track a functions exection
+        i.e. a fake function
+
+    
+
+    
+
+*/
